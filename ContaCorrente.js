cf025c30f95ebb67db119f025997c931ed6ffb6f
Natália Sens Weise <nataliaweise@gmail.com>
2021-11-19 18:03:52 -0300

    Creating classes

---
+++ ContaCorrente.js
@@ -0,0 +1,20 @@
+import { Conta } from "./Conta.js";
+
+export class ContaCorrente extends Conta {
+  static numeroDeContas = 0;
+  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+
+  constructor(agencia, cliente) {
+    super(0, cliente, agencia);
+    ContaCorrente.numeroDeContas += 1;
+  }
+
+  sacar(valor) {
+    let taxa = 1.1;
+    const valorSacado = taxa * valor;
+    if (this._saldo >= valorSacado) {
+      this._saldo -= valorSacado;
+      return valorSacado;
+    }
+  }
+}
